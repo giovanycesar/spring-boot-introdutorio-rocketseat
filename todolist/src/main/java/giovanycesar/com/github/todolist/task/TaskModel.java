bfef6960a5f6e61c14605b5f8951603dfdd65460
Giovany Almeida <gvncesar@gmail.com>
2025-11-13 20:34:56 -0300

    Tabela de Task e realizando Filter

---
+++ todolist/src/main/java/giovanycesar/com/github/todolist/task/TaskModel.java
@@ -0,0 +1,40 @@
+package giovanycesar.com.github.todolist.task;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.annotations.Type;
+import org.hibernate.type.SqlTypes;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity(name = "tb_tasks")
+public class TaskModel {
+
+    @Id
+    @GeneratedValue
+    @JdbcTypeCode(SqlTypes.CHAR)
+    private UUID id;
+
+    private String description;
+
+    @Column(length = 50)
+    private String title;
+    private LocalDateTime startAt;
+    private LocalDateTime endAt;
+    private String priority;
+
+    @JdbcTypeCode(SqlTypes.CHAR)
+    private UUID userId;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+}
--- todolist/src/main/java/giovanycesar/com/github/todolist/user/UserModel.java
+++ todolist/src/main/java/giovanycesar/com/github/todolist/user/UserModel.java
@@ -4,18 +4,21 @@ import java.time.LocalDateTime;
 import java.util.UUID;
 
 import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.annotations.Type;
+import org.hibernate.type.SqlTypes;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity(name = "tb_users")
 public class UserModel {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
+    @GeneratedValue
+    @JdbcTypeCode(SqlTypes.CHAR)
     private UUID id;
 
     @Column(unique = true)
